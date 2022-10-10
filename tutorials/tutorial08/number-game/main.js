daad1ee418b09da9ea4f18fa03dc0e1a60a14b5b
Karoline <kcarl@unca.edu>
2022-10-10 13:35:37 -0400

    tutorial 8

--- tutorials/tutorial08/number-game/main.js
+++ tutorials/tutorial08/number-game/main.js
@@ -2,8 +2,25 @@
 const secret = Math.floor(Math.random() * 99) + 1;
 document.querySelector('#secret').innerHTML = secret;
 
+let counter = 0;
 
-function check() {
-    // your code here.
-    console.log("your code here!");
+function check() { 
+    let userGuess = document.querySelector('#guess').value;
+    console.log(userGuess);
+    userGuess = Number(userGuess);
+
+    if(userGuess === secret){
+        console.log('you win!');
+        document.querySelector('#message').innerHTML = 'you win!'
+        document.querySelector('#celebrate').className = '';
+    } else if (userGuess < secret){
+        console.log('to low!');
+        document.querySelector('#message').innerHTML = 'to low!'
+    } else{
+        console.log("to high!")
+        document.querySelector('#message').innerHTML = 'to high!'
+    }
+    counter ++;
+    document.querySelector('#num-guesses').innerHTML = 'you have guessed ' + counter + ' times';
+
 }
