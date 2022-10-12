794597d068403e2ff4ffe73de31b36900a5271b6
Karoline <kcarl@unca.edu>
2022-10-12 14:51:42 -0400

    hw2

--- tutorials/tutorial08/circles/sketch.js
+++ tutorials/tutorial08/circles/sketch.js
@@ -5,11 +5,20 @@ function setup() {
 
     // fill('red');
     noFill();
-    circle(100, 200, 50);
-    circle(100, 250, 50);
-    circle(100, 300, 50);
-    circle(100, 350, 50);
-    circle(100, 400, 50);
+    let counter = 0;
+    while (counter < 800) {
+        if(counter % 3 == 0) {fill('hotpink')}
+        else if(counter % 2 === 0){
+            fill('teal')
+        } else {fill('white')}
+        circle(canvasWidth/2, 0 + counter * 10, counter * 10);
+        counter +=1;
+    }
+    //circle(100, 200+ 1 * 50, 50);
+    //circle(100, 250 + 2 * 50, 50);
+    //circle(100, 300 + 3 * 50, 50);
+    //circle(100, 350 + 5 * 50, 50 );
+    //circle(100, 400 + 0* 50, 50);
 
     drawGrid(canvasWidth, canvasHeight);
 }
