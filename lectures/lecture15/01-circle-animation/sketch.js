5ed197fb277138b5778ca32805840de2d3c3df43
Karoline <kcarl@unca.edu>
2022-10-19 19:36:21 -0400

    landscape

--- lectures/lecture15/01-circle-animation/sketch.js
+++ lectures/lecture15/01-circle-animation/sketch.js
@@ -1,10 +1,10 @@
 const circleData = [
-    {x: 500, y: 50, d: 100, speedX: 3, speedY: 3},
-    {x: 300, y: 150, d: 50, speedX: 2, speedY: 0},
-    {x: 400, y: 400, d: 25, speedX: 0, speedY: 1},
-    {x: 40, y: 520, d: 80, speedX: 2, speedY: 0},
-    {x: 140, y: 120, d: 150, speedX: 0, speedY: -0.5},
-    {x: 350, y: 350, d: 70, speedX: 1, speedY: 1}
+    {x: 500, y: 50, d: 100, speedX: 3, speedY: 3, color: 'teal'},
+    {x: 300, y: 150, d: 50, speedX: 2, speedY: 0, color: 'green'},
+    {x: 400, y: 400, d: 25, speedX: 0, speedY: 1, color: 'yellow'},
+    {x: 40, y: 520, d: 80, speedX: 2, speedY: 0, color: 'light blue'},
+    {x: 140, y: 120, d: 150, speedX: 0, speedY: -0.5, color: 'pink' },
+    {x: 350, y: 350, d: 70, speedX: 1, speedY: 1, color: 'purple'}
 ];
 
 const canvasWidth = window.innerWidth;
@@ -30,9 +30,18 @@ const ball = {
 // every time the animation loop invokes the draw() function:
 function draw() {
     clear();
-    fill('hotpink');
-    circle(ball.x, ball.y, ball.d);
+   
+    
+    // let i = 0;
+    // while (i < circleData.length){
+        for(let i = 0; i<circleData.length; i++){
+        const ball = circleData[i]; 
+        fill(ball.color);
+        circle(ball.x, ball.y, ball.d);
     ball.x += ball.speedX;
     ball.y += ball.speedY;
-    drawGrid(canvasWidth, canvasHeight);
+    i++;
 }
+   drawGrid(canvasWidth, canvasHeight);
+    }
+    
