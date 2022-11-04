81b6bbc3c04cb84618c142d37df18888388d0d79
Karoline <kcarl@unca.edu>
2022-11-04 12:59:54 -0400

    circle fill color

--- tutorials/tutorial12/exercise02/main.js
+++ tutorials/tutorial12/exercise02/main.js
@@ -6,10 +6,20 @@ function setup() {
 
     // function invocations goes here:
 
-
+    drawShape(100, 100, 150, '#db5461', '#102e4a');
+    drawShape(200, 200, 75, '#102e4a', '#8093f1');
+    drawShape(100, 325, 100, '#8093f1', '#7fb285');
+    drawShape(250, 375, 125, '#7fb285', '#0bc9cd');
+    drawShape(450, 200, 250, '#0bc9cd', '#db5461');
 
     drawGrid(canvasWidth, canvasHeight);
 }
 
 
 // function definition goes here:
+function drawShape(centerX, centerY, size, fillColor, fillcolor2) {
+    fill(fillColor);
+    circle(centerX, centerY, size);
+    fill(fillcolor2);
+    circle(centerX, centerY, size * .5);
+}
